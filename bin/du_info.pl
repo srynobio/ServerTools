bb8c8540e8a4897a02d5dc19a549c981e3dbbf5b
shawn rynerson <shawn.rynearson@gmail.com>
2013-12-18 22:14:32 -0700

    first commit

---
+++ bin/du_info.pl
@@ -0,0 +1,49 @@
+!/usr/bin/perl
+use warnings;
+use strict;
+use Parallel::ForkManager;
+
+my $pm = Parallel::ForkManager->new('6');
+
+my $output = $ARGV[0] or die;
+my $mail   = 'lab@yandell-lab.org';
+
+my $host = `hostname`;
+chomp($host);
+my @diskSpace = `df -h |column -t`;
+
+my (@warn, @drive);
+foreach my $df (@diskSpace) {
+  chomp $df;
+
+  next if $df =~ /^Filesystem|^pathway|^bdarchive/;
+  $df =~ s/\s+/:/g;
+
+  my @results = split ":", $df;
+  $results[4] =~ s/\%$//g;
+
+  next unless (int $results[4]);
+
+  if ( $results[4] >= 95 ) {
+    push @warn, $results[0];
+    push @drive, $results[-1];
+  }
+}
+
+if ( @warn ) {
+
+  my $count;
+  foreach my $drive (@drive) {
+      my $cmd = "du $drive |sort -rn |head -25 > du_sort_" . ++$count;
+      $pm->start and next;
+      `$cmd`;
+      $pm->finish;
+    }
+    $pm->wait_all_children;
+}
+
+`cat du_sort_* > $output`;
+system("mail -s \"du_info from: $host the following drives are > 95% full\" $mail < $output");
+sleep(60);
+`rm du_sort_* du_output`;
+
