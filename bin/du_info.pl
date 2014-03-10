74b2bf032808f77d9bbd4b97ee214c525210a3d3
shawn rynerson <shawn.rynearson@gmail.com>
2014-03-10 10:11:46 -0600

    added du root sort to look at home

--- bin/du_info.pl
+++ bin/du_info.pl
@@ -3,50 +3,57 @@ use warnings;
 use strict;
 use Parallel::ForkManager;
 
-my $pm   = Parallel::ForkManager->new('6');
+my $pm = Parallel::ForkManager->new('6');
 my $mail = $ARGV[0] or die "Please add email\n";
 
 my $host = `hostname`;
 chomp($host);
 my @diskSpace = `df -h |column -t`;
 
-my (@warn, @drive);
+my ( @warn, @drive );
 foreach my $df (@diskSpace) {
-  chomp $df;
+    chomp $df;
 
-  next if $df =~ /^Filesystem|^pathway|^bdarchive|^crosman|^archive/;
-  $df =~ s/\s+/:/g;
+    print $df, "\n";
 
-  my @results = split ":", $df;
-  $results[4] =~ s/\%$//g;
+    next if $df =~ /^Filesystem|^pathway|^bdarchive|^crosman|^archive/;
+    $df =~ s/\s+/:/g;
 
-  next unless (int $results[4]);
+    my @results = split ":", $df;
+    $results[4] =~ s/\%$//g;
 
-  if ( $results[4] >= 85 ) {
-    push @warn, $results[0];
-    push @drive, $results[-1];
-  }
+    next unless ( int $results[4] );
+
+    if ( $results[4] >= 85 ) {
+        push @warn,  $results[0];
+        push @drive, $results[-1];
+    }
 }
 
-if ( @warn ) {
+if (@warn) {
 
-  my $count;
-  my @du_collect;
-  foreach my $drive (@drive) {
-    my $du_file = "du_sort_" . ++$count;
-    my $cmd = "du $drive |sort -rn |head -50 > $du_file";
-    push @du_collect, $du_file;
-    $pm->start and next;
-    `$cmd`;
-    $pm->finish;
-  }
-  $pm->wait_all_children;
+    my $count;
+    my @du_collect;
+    foreach my $drive (@drive) {
 
-  foreach my $dirs (@du_collect) {
-    chomp $dirs;
-    system("mail -s \"du_info from: $host the following drives are > 85% full\" $mail < $dirs");
-    sleep(60);
-  }
-  `rm du_sort_*`;
+        if ( $drive =~ /\// ) {
+            $drive =~ s/\//\/home/;
+        }
+
+        my $du_file = "du_sort_" . ++$count;
+        my $cmd     = "du $drive |sort -rn |head -50 > $du_file";
+        push @du_collect, $du_file;
+        $pm->start and next;
+        `$cmd`;
+        $pm->finish;
+    }
+    $pm->wait_all_children;
+
+    foreach my $dirs (@du_collect) {
+        chomp $dirs;
+        system("mail -s \"du_info from: $host the following drives are > 85% full\" $mail < $dirs");
+        sleep(60);
+    }
+    `rm du_sort_*`;
 }
 
