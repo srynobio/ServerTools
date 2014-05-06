05736bfe71a911e59d4964d828417395d6991905
srynearson <srynearson@newrepublic.genetics.utah.edu>
2014-05-05 22:30:12 -0600

    updated load_average script

--- bin/load_monitor.pl
+++ bin/load_monitor.pl
@@ -1,8 +1,7 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 use strict;
 use warnings;
 use IO::File;
-use Storable;
 
 my $mail = $ARGV[0];
 my $host = `hostname`;
@@ -16,55 +15,28 @@ my @av = split /\s+/, $loadavg;
 if ( $pros < $av[0] ) {
     my @list = `ps -eo pid,pcpu,user,args --sort pcpu`;
 
-    if ( -e 'highCPU.store' ) {
+    my @overload;
+    foreach my $job (@list) {
+        chomp $job;
+        $job =~ s/^\s+//g;
+        next if ( $job =~ /PID/ );
 
-        my $highcpu = retrieve('highCPU.store');
+        my @groups = split / /, $job;
+        next if ( $groups[3] eq 'root' );
+        next if ( !$groups[1] or $groups[1] < 90 );
 
-        foreach my $over ( keys %{$highcpu} ) {
-            chomp $over;
-            `kill -9 $over`;
-        }
-        `rm highCPU.store`;
-        exit(0);
+        push @overload, $job;
     }
-
-    my %overload;
-    foreach my $jobs (@list) {
-        chomp $jobs;
-        $jobs =~ s/^\s+//g;
-        next if ( $jobs =~ /PID/ );
-
-        my @groups = split / /, $jobs;
-        next if ( $groups[3] eq 'root');
-
-        next unless $groups[1] > 300;
-
-        $overload{ $groups[0] } = $jobs;
-    }
-    my @pids = keys %overload;
-
-    store \%overload, 'highCPU.store';
 
     my $OUT = IO::File->new( 'WARNfile', 'a+' ) or die;
-    my $comment = "Due to high CPU load the following jobs will be KILLED in one hour if left in current state\n\n";
+    my $comment =
+	"Due to high CPU load average this WARNING email has been sent out.  Please review your server usage, "
+      . "as it can affect job completion.\n\n\n";
     print $OUT $comment;
+    map { print $OUT $_, "\n" } @overload;
 
-    foreach my $over ( keys %overload ) {
-        chomp $over;
-        map { print $OUT $_, "\n" } $overload{$over};
-    }
-
-    print $OUT "\n\n";
-    while (<DATA>) {
-        print $OUT $_;
-    }
-
-    system("mail -s \"CPU load is critical and above capacity for the following jobs on $host\" $mail < WARNfile");
-    `rm WARNfile`;
+    system(
+	"mail -s \"CPU load is critical and above capacity on $host\" $mail < WARNfile"
+    );
 }
 
-__DATA__
-On Her Majesty's Secret Service
-
-Draco: My apologies for the way you were brought here. I wasn't sure you'd accept a formal invitation.
-Bond:  There's always something formal about the point of a pistol.
