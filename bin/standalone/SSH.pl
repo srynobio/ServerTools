5018e8e41a942dc88931e56ab61b6b8e19b60caa
Shawn Rynearson <shawn.rynearson@gmail.com>
2015-01-19 14:55:26 -0700

    Update SSH.pl
    fixed bug

--- bin/standalone/SSH.pl
+++ bin/standalone/SSH.pl
@@ -8,7 +8,7 @@ use Data::Dumper;
 
 my $ssh = Net::SSH::Expect->new (
 	host => 'ugp.genetics.utah.edu',
-	password => '*Gq~O$+F6\h3f)//uYnNz8gs(%8oc~#(Vi4do,6NIhHKQxlFUC@j{ER~.jU@MS1n8em:_T5$LGWRGX7}1<]ys,Oj>WYKqWan_zP_',
+	password => '***************',
 	user => 'srynearson',
 	raw_pty => 1
 );
